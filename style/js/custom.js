856f73376bfc9dd94a9dce952459d7ffd0cd6eb7
jacobparker <jacob.parker04@gmail.com>
2020-01-19 22:38:02 -0700

    first commit

---
+++ style/js/custom.js
@@ -0,0 +1,74 @@
+/*$(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          var box = $('.header-text').height();
+          var header = $('header').height();
+
+            $("header").addClass("background-header");
+          } else {
+            $("header").removeClass("background-header");
+          }
+        });
+*/
+
+/*Typewriter Effect*/
+var i = 0,
+    a = 0,
+    isBackspacing = false,
+    isParagraph = false;
+
+var textArray = [
+  "creative", 
+  "passionate",
+  "detailed",
+  "innovative",
+  "determined"
+];
+
+var speedForward = 100,
+    speedWait = 1750, 
+    speedBackspace = 100; 
+
+//Invoke function
+typeWriter("output", textArray);
+
+function typeWriter(id, ar) {
+  var element = $("#" + id),
+      aString = ar[a],
+      eHeader = element.children("h2");
+  
+  if (!isBackspacing) {
+    
+    if (i < aString.length) {
+
+        if (!isParagraph) {
+          eHeader.text(eHeader.text() + aString.charAt(i));
+        } 
+        i++;
+        setTimeout(function(){ typeWriter(id, ar); }, speedForward);
+
+    } else if (i == aString.length) {
+      isBackspacing = true;
+      setTimeout(function(){ typeWriter(id, ar); }, speedWait);
+    }
+    
+    //If isBackspacing = true
+  } else {
+    
+    if (eHeader.text().length > 0) {
+      
+      if (eHeader.text().length > 0) {
+        eHeader.addClass("cursor");
+        eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
+      }
+      setTimeout(function(){ typeWriter(id, ar); }, speedBackspace);
+    
+    } else { 
+      isBackspacing = false;
+      i = 0;
+      isParagraph = false;
+      a = (a + 1) % ar.length; //Moves to next position in array, loops back to 0
+      setTimeout(function(){ typeWriter(id, ar); }, 50);
+      
+    }
+  }
+}
